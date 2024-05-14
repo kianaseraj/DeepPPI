c3d600477ef26e316c6261dcbd859b411ae7507a
Kiana <kianaseraj98@gmail.com>
2024-05-14 16:11:51 +0200

    defining test_sequence_partition

--- test_deep_ppi.py
+++ test_deep_ppi.py
@@ -8,3 +8,8 @@ def test_AAC():
 
 def test_hydrophobicity_descriptor():
     assert feature_extractor.hydrophobicity_descriptor(SEQUENCE) == "1112222111"
+
+
+def test_sequence_partition():
+    expected = ("RK", "ES", "TP", "HDQN", "RKEST", "PHDQN")
+    assert feature_extractor.sequence_partition(SEQUENCE) == expected
